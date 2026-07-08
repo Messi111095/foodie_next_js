850c2fdccaadf1a7ff114178257d2e6d03851923
Messi111095 <ashutoshkumar111095@gmail.com>
2026-07-08 17:16:29 +0530

    made on 08-07-2026

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,26 @@
-export default function MealDetailsPage(){
-    return <h1>Meal Details</h1>
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export default function MealDetailsPage({params}){
+    const meal = getMeal(params.mealSlug);
+    return (
+        <>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image fill/>
+            </div>
+            <div className={classes.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={classes.creator}>By <a href={`mailto:${meal.creatorEmail}`}>{meal.creator}</a></p>
+                <p className={classes.summary}>{meal.summary}</p>
+            </div>
+        </header>
+        <main>
+            <p className={classes.instructions} dangerouslySetInnerHTML={{
+                __html: meal.instructions,
+            }}>Instructions</p>
+        </main>
+        </>
+    );
 }
