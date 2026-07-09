6519c16d23a84a07afdff1c6dddd961f292daca2
Messi111095 <ashutoshkumar111095@gmail.com>
2026-07-09 12:35:40 +0530

    date 09-07-2026

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@
 export default function NotFound() {
   return (
     <main className="not-found">
-      <h1>Not Found</h1>
+      <h1>Meals Not Found</h1>
       <p>Unfortunately, we could not find the page you were looking for.</p>
     </main>
   );
